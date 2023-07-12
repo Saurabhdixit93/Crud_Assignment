b907356a7b15e3d43748c46add079d57cbf6b62a
Saurabh Dixit <smartds2550@gmail.com>
2023-07-12 02:19:56 +0000

    Add new file

---
+++ index.js
@@ -0,0 +1,21 @@
+// importing express
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = 5000;
+const app = express();
+// for json
+app.use(express.json());
+// form data
+app.use(express.urlencoded({ extends: true }));
+// using routes
+app.use("/", require("./routers"));
+//not found route
+app.use("*", (req, res) => {
+  return res.status(404).json({
+    PageNotFound: "Can't Found Page",
+  });
+});
+// starting server
+app.listen(PORT, () => {
+  console.log(`Successfully Running Server in PORT: ${PORT}`);
+});
